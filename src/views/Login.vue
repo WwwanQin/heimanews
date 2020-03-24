2147957283d9207aed8137b58749835403e02082
yusen <xm36447864@126.com>
2020-03-24 17:33:57 +0800

    第二次提交

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p class="title">{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg:'登录页面'
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .title{
+        font-size: 17px;
+        font-weight: 700;
+    }
+</style>
